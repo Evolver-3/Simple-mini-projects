bfff05bb13d2cd48e0c1ec79f4172a4102efe2a9
Ashu Singh <Ashusingh2201@gmail.com>
2025-12-02 15:05:04 +0530

    timer done

--- Quiz/src/components/Timer.jsx
+++ Quiz/src/components/Timer.jsx
@@ -2,26 +2,35 @@ import {useState,useEffect} from "react"
 const Timer=()=>{
 
   const [leftTime,setLeftTime]=useState(10);
+  const [displayTime,setDisplayTime]=useState('')
 
  useEffect(()=>{
-   let intervalId=setInterval(()=>{
-     setLeftTime(p=>{
+  let intervalId=setInterval(()=>{
+    setLeftTime(p=>{
       if(p<=0){
-        clearInterval(intervalId);
+        clearInterval(intervalId)
         return 0;
-      }else{
-        return p-1;
-      }
-   })
+       }
+       else{
+        return p-1; 
+       }
+  })
   },1000)
 
-  return()=>
-  clearInterval(intervalId)
+  return()=>clearInterval(intervalId)
+
  },[])
+
+ useEffect(()=>{
+  let setTime=(`${Math.floor(leftTime/60).toString().padStart(2,"0")} : ${(leftTime%60).toString().padStart(2,"0")} sec`);
+
+  setDisplayTime(setTime)
+
+ },[leftTime])
 
   return(
   <div>
-    🕗 Time left: {leftTime}
+    🕗 Time left: {displayTime}
   </div>
 )
 }
