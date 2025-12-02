a2a376c56d5bdb1c8bec0db2ab596667371c4e10
Ashu Singh <Ashusingh2201@gmail.com>
2025-12-02 16:20:57 +0530

    updated quiz app

--- Quiz/src/components/QuizArea.jsx
+++ Quiz/src/components/QuizArea.jsx
@@ -1,20 +1,47 @@
-import Timer from "./Timer"
+import { useState } from "react";
+import Timer from "./Timer";
+import ques from './ques.json'
+import { useNavigate } from "react-router-dom";
 const QuizArea=()=>{
+
+  const [currentIndex,setIndex]=useState(0);
+  const [score,setScore]=useState(0);
+  const navigate=useNavigate();
+
+  const handleClick=(selected)=>{
+    if(selected===ques[currentIndex].answer){
+      setScore(prev=>prev+1)
+    }
+
+    if(currentIndex<ques.length-1){
+      setIndex(prev=>prev+1)
+    }else{
+      navigate("/Result",{state:{score}})
+    }
+    
+  }
+
   return(
   <div  className="container bg-gray-400 items-center p-10  w-full h-[100vh]">
     <div className="flex flex-col items-center justify-center gap-10 bg-white rounded-lg px-10 py-5">
-      <p className="text-[20px] font-bold ">Question</p>
+      {ques[currentIndex].question}
 
       <span><Timer/></span>
 
       <div className="flex justify-between w-full text-sm text-gray-500">
-      <p>Question <span> 4 </span> of <span> 5 </span></p>
+      <p>Question <span>{(currentIndex+1)}</span> of <span>{(ques.length)}</span></p>
 
-      <p>Score: <span>2</span></p>
+      <p>Score: <span>{(score)}</span></p>
       </div>
 
       <div className="w-full">
-         <div className="w-full bg-orange-300 h-auto text-white rounded-lg px-2">heool</div>
+         <div className="w-full  h-auto text-white rounded-lg px-2 flex flex-col gap-2">
+          {ques[currentIndex].options.map((option)=>{
+            return <button key= {option}className="bg-orange-300 rounded-lg text-gray-600 hover:bg-orange-400 hover:text-black" onClick={()=>{
+              handleClick(option)
+            }}> {option}</button>
+          })}
+         </div>
       </div>
 
     </div>
--- Quiz/src/components/Result.jsx
+++ Quiz/src/components/Result.jsx
@@ -1,13 +1,22 @@
+
+import { useLocation, useNavigate } from "react-router-dom"
+
 const Result=()=>{
+
+  const {state} =useLocation();
+
+  const score =state?.score ||0;
+
+  const nav=useNavigate();
   return(
   <div className="container bg-gray-400  items-center p-10 w-full h-[100vh] ">
 
     <div className="flex flex-col items-center justify-center gap-10 bg-white rounded-lg px-10 py-5">
     <h2 className="text-orange-500 text-[30px] font-bold">Quiz Results</h2>
     <div className="bg-orange-100 w-auto py-10 px-10 rounded-lg">
-      <p>Your Score is <span>1</span> out of <span>4</span></p>
+      <p>Your Score is <span>{score}</span> out of <span>10</span></p>
     </div>
-    <button className="bg-orange-500 text-white px-3 py-4 rounded-md hover:text-gray-400 hover:bg-orange-600">Restart Quiz</button>
+    <button className="bg-orange-500 text-white px-3 py-4 rounded-md hover:text-gray-400 hover:bg-orange-600" onClick={()=>nav("/")}>Restart Quiz</button>
     </div>
 
   </div>
